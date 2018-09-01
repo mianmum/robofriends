0dbed2677de0bc86efbb56c72686c85f594e8043
Mumin Mian <35271260+mianmum@users.noreply.github.com>
2018-09-01 15:58:05 -0400

    Add scroll and fetch from API instead of local file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll.js';
+import './App.css';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     };
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users})
+      });
   };
 
   onSearchChange = (event) => {
@@ -23,9 +33,11 @@ class App extends Component {
     console.log(filteredRobots);
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>  
       </div>
     );
   };
